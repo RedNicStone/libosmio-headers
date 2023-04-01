2717ccf8ac29edc5f6bb8e2c1b20680f6fad5855
RedNicStone <nic@struktur.de>
2023-04-01 16:00:37 +0100

    Fixed issues regarding cmake file and the visibility of header files

---
+++ include/libosmio.h
@@ -0,0 +1,16 @@
+//
+// Created by RedNicStone on 09/03/23.
+//
+
+#ifndef LIBOSMIO_H
+#define LIBOSMIO_H
+
+#define OS_VERSION_1_0 1
+
+#include "libosmio/handles.h"
+#include "libosmio/enums.h"
+#include "libosmio/structures.h"
+#include "libosmio/functions.h"
+#include "libosmio/icd.h"
+
+#endif //LIBOSMIO_H
